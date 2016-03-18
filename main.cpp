d5dacb3e911cbfe9d136b14d5e5209e57a37db93
Ryan Quinn <fowardingtoyou@gmail.com>
2016-03-18 18:51:03 -0400

    Made motion fluid

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include "chip8.h"
 #include <iostream>
+#include <unistd.h>
 
 using namespace std;
 
@@ -9,11 +10,15 @@ int main(int argc, char *argv[])
   Chip8 emu;
   bool succ = emu.loadGame(argv[1]);
   for (int i = 0; i < 700; i++)
-    emu.executeNextOpcode();
-  for (int i = 0; i <= 32 - 1; i++)
   {
-    for (int j = 0; j <= 64 - 1; j++)
-      cout << static_cast<int>(emu.screen[j][i]);
-    cout << endl;
+    emu.executeNextOpcode();
+    for (int i = 0; i <= 32 - 1; i++)
+    {
+      for (int j = 0; j <= 64 - 1; j++)
+        cout << static_cast<int>(emu.screen[j][i]);
+      cout << endl;
+    }
+    usleep(10000);
   }
+
 }
